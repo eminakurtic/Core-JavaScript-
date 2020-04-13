764ce651b2f965adaefe92fedb5843d75e730fa0
eminakurtic <57722363+eminakurtic@users.noreply.github.com>
2020-04-13 14:01:13 +0200

    Add files via upload

---
+++ 1/script.js
@@ -0,0 +1,9 @@
+document.write("Izlaz iz eksternog JavaScript-a!");
+alert("Izlaz unutar prozora!");
+confirm("Da ili Ne?");
+var x = prompt("Unesi godinu...");
+alert(x);
+console.log("Izlaz u konzoli!");
+document.getElementById("naslov").innerHTML = "Sadržaj naslova!";
+
+var promjenljiva;
