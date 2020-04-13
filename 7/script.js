cb16ddbfc41f5a5f5e0fb9c0c11b0d45696ac94a
eminakurtic <57722363+eminakurtic@users.noreply.github.com>
2020-04-13 14:03:34 +0200

    Add files via upload

---
+++ 7/script.js
@@ -0,0 +1,10 @@
+var d = new Date();
+var sat = d.getHours();
+
+if(sat < 12){
+    document.write("Dobro jutro radni narode.");
+}else if(sat > 11 && sat < 18){
+    document.write("Dobar dan!");
+}else{
+    document.write("Dobro veče!");
+}
