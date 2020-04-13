3340b5ea28c8688506351e179830973538443fbc
eminakurtic <57722363+eminakurtic@users.noreply.github.com>
2020-04-13 14:02:28 +0200

    Add files via upload

---
+++ 3/script.js
@@ -0,0 +1,13 @@
+/*
+var a = prompt("a?");
+var b = prompt("b?");
+
+a = parseInt(a);
+b = parseInt(b);
+
+var result = a + b;
+alert(result);
+*/
+
+// Skraćena varijanta
+alert(parseInt(prompt("a?")) + parseInt(prompt("b?")));
